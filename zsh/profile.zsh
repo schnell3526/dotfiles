a457073e536500a9ca2790896b5136d9a351aea7
acc12730ud <acc12730ud@es1.abci.local>
2022-05-18 11:21:28 +0900

    edit profile.zsh

--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -2,19 +2,22 @@ if [[ ${HOSTNAME} =~ abci ]]; then
 # ABCI用の設定
     # local設定用path
     LOCAL_PATH=${HOME}/.local
+
     # ncurses
     NCURSES_INC=${LOCAL_PATH}/include/ncurses
+
     export CPATH=$NCURSES_INC:$CPATH
     export LD_LIBRARY_PATH=${LOCAL_PATH}/lib:$LD_LIBRARY_PATH
     export PKG_CONFIG_PATH=${LOCAL_PATH}/lib/pkgconfig:$PKG_CONFIG_PATH
+
     # .localにパスを通す
     export PATH=${LOCAL_PATH}/bin:$PATH
+
     # 共有フォルダにパスを通す
     export PATH=${DIR_GROUP}/share/usr/bin:$PATH
 
     # よく使うモジュールをロード
-    module load gcc/9.3.0
-    python/3.8
+    module load gcc/9.3.0 python/3.8
 elif [[ $(uname) = "Darwin" ]]; then # OSX
     # 環境変数
     export LANG=ja_JP.UTF-8
